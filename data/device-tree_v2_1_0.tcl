af8e453e3c6d8bf6d0fa9d063504af0d46bad624
Michal Simek <monstr@monstr.eu>
2010-12-14 10:43:22 +0000

    device-tree: phy_count support, add phy supported functions
    git-svn-id: https://developer.petalogix.com/pr/internal/petalinux/branches/petalinux-v1.00-devel@8916 252c5255-be06-0410-b2d7-f4a01eabda39

--- data/device-tree_v2_1_0.tcl
+++ data/device-tree_v2_1_0.tcl
@@ -50,6 +50,7 @@ set overrides {}
 set serial_count 0
 set ethernet_count 0
 set alias_node_list {}
+set phy_count 0
 
 #
 # How to use generate_device_tree() from another MLD
@@ -712,7 +713,7 @@ proc slave_ll_temac_port {slave intc index} {
 	#
 	variable ethernet_count
 	variable alias_node_list
-	set alias_node [list ethernet$ethernet_count aliasref $name]
+	set alias_node [list ethernet$ethernet_count aliasref $name $ethernet_count]
 	lappend alias_node_list $alias_node
 	incr ethernet_count
 
@@ -875,6 +876,9 @@ proc change_nodename {nodetochange oldname newname} {
 }
 
 proc gener_slave {node slave intc} {
+	variable phy_count
+	variable mac_count
+
 	set name [xget_hw_name $slave]
 	set type [xget_hw_value $slave]
 	switch -exact $type {
@@ -1033,15 +1037,23 @@ proc gener_slave {node slave intc} {
 			#
 			variable ethernet_count
 			variable alias_node_list
-			lappend alias_node_list [list ethernet$ethernet_count aliasref $name]
+			lappend alias_node_list [list ethernet$ethernet_count aliasref $name $ethernet_count]
 			incr ethernet_count
 
 			# 'network' type
 			set ip_tree [slaveip_intr $slave $intc [interrupt_list $slave] "ethernet" [default_parameters $slave]]
 			set ip_tree [tree_append $ip_tree [list "device_type" string "network"]]
-			variable mac_count
 			set ip_tree [tree_append $ip_tree [list "local-mac-address" bytesequence [list 0x00 0x0a 0x35 0x00 0x00 $mac_count]]]
 			incr mac_count
+
+			if {$type == "xps_ethernetlite" || $type == "axi_ethernetlite"} {
+				set has_mdio [scan_int_parameter_value $slave "C_INCLUDE_MDIO"]
+				if {$has_mdio == 1} {
+					set phy_name "phy$phy_count"
+					set ip_tree [tree_append $ip_tree [list "phy-handle" labelref $phy_name]]
+					set ip_tree [tree_append $ip_tree [gen_mdiotree]]
+				}
+			}
 
 			lappend node $ip_tree
 		}
@@ -1924,6 +1936,26 @@ proc scan_int_parameter_value {ip_handle name} {
 		binary scan [binary format "B*" $tail] "I*" value
 	}
 	return [expr $value]
+}
+
+proc gen_phytree {} {
+	variable phy_count
+
+	set phy_name [format_ip_name phy 7 "phy$phy_count"]
+	set phy_tree [list $phy_name tree {}]
+	set phy_tree [tree_append $phy_tree [list "reg" int 7]]
+	set phy_tree [tree_append $phy_tree [list "device_type" string "ethernet-phy"]]
+	set phy_tree [tree_append $phy_tree [list "compatible" string "marvell,88e1111"]]
+
+	incr phy_count
+	return $phy_tree
+}
+
+proc gen_mdiotree {} {
+	set mdio_tree [list "mdio" tree {}]
+	set mdio_tree [tree_append $mdio_tree [list \#size-cells int 0]]
+	set mdio_tree [tree_append $mdio_tree [list \#address-cells int 1]]
+	return [tree_append $mdio_tree [gen_phytree]]
 }
 
 proc format_name {par_name} {
