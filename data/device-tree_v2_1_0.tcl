94d183c8ccbc8369e669fe777e8a614f689a519c
Michal Simek <monstr@monstr.eu>
2011-05-27 11:25:22 +0200

    device-tree: Update headers
    No code changes.
    
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- data/device-tree_v2_1_0.tcl
+++ data/device-tree_v2_1_0.tcl
@@ -3,7 +3,7 @@
 #
 # (C) Copyright 2007-2008 Xilinx, Inc.
 # Based on original code:
-# (C) Copyright 2007-2010 Michal Simek
+# (C) Copyright 2007-2011 Michal Simek
 # (C) Copyright 2007-2011 PetaLogix Qld Pty Ltd
 #
 # Michal SIMEK <monstr@monstr.eu>
@@ -362,7 +362,7 @@ proc headerc {ufile generator_version} {
 	puts $ufile " * Device Tree Generator version: $generator_version"
 	puts $ufile " *"
 	puts $ufile " * (C) Copyright 2007-2008 Xilinx, Inc."
-	puts $ufile " * (C) Copyright 2007-2010 Michal Simek"
+	puts $ufile " * (C) Copyright 2007-2011 Michal Simek"
 	puts $ufile " * (C) Copyright 2007-2011 PetaLogix Qld Pty Ltd"
 	puts $ufile " *"
 	puts $ufile " * Michal SIMEK <monstr@monstr.eu>"
