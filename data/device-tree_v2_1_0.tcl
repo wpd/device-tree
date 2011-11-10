29dbd64232619ff6e6e78473bae287e648ea4b55
Michal Simek <michal.simek@petalogix.com>
2011-11-10 08:30:19 +0100

    bsp: device-tree: Fix MDM hack for serial@ generation
    Fix "dts-generator: tag UART-capable MDMs as serial"
    (sha1: 32ddb06f7f7a58851ed7eeaf45a80f59f110c444)
    
    Signed-off-by: Michal Simek <michal.simek@petalogix.com>

--- data/device-tree_v2_1_0.tcl
+++ data/device-tree_v2_1_0.tcl
@@ -896,22 +896,13 @@ proc gener_slave {node slave intc} {
 		"mdm" -
 		"opb_mdm" {
 			# Microblaze debug
+
 			# Check if uart feature is enabled
 			set use_uart [xget_hw_parameter_value $slave "C_USE_UART"]
-			# HACK - if it's got the UART then tag it as a serial
-			# device in the device tree.
-			if { "$use_uart" == "1" } {
-				set type "serial"
-			} else {
-				set type "debug"
-			}
-			# EDK 11.4 disables PLB connection when USE_UART is disabled that's why whole node won't be generated
-			# Only bus connected IPs are generated
-			set ip_tree [slaveip_intr $slave $intc [interrupt_list $slave] "${type}" [default_parameters $slave] "" "" "xlnx,xps-uartlite-1.00.a" ]
-			#"C_MB_DBG_PORTS C_UART_WIDTH C_USE_UART"
-
 			if { "$use_uart" == "1" } {
 				check_console_irq $slave $intc
+
+				set ip_tree [slaveip_intr $slave $intc [interrupt_list $slave] "serial" [default_parameters $slave] "" "" "xlnx,xps-uartlite-1.00.a" ]
 
 				variable alias_node_list
 				global consoleip
@@ -924,6 +915,11 @@ proc gener_slave {node slave intc} {
 					lappend alias_node_list [list serial$serial_count aliasref $name $serial_count]
 					set ip_tree [tree_append $ip_tree [list "port-number" int $serial_count]]
 				}
+			} else {
+				# EDK 11.4 disables PLB connection when USE_UART is disabled that's why whole node won't be generated
+				# Only bus connected IPs are generated
+				set ip_tree [slaveip_intr $slave $intc [interrupt_list $slave] "debug" [default_parameters $slave] "" "" "xlnx,xps-uartlite-1.00.a" ]
+				#"C_MB_DBG_PORTS C_UART_WIDTH C_USE_UART"
 			}
 			lappend node $ip_tree
 		}
