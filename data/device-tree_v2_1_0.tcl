5895e7adcfb498800e6c440e22bf33ab928a70b9
Michal Simek <michal.simek@xilinx.com>
2013-03-06 17:36:50 +0100

    zynq: ddrc: Add ps7-ddrc compatible property
    All zynq IPs provide 1.00.a version which is
    pretty long and useless. Use shorter version.
    
    Generated node:
    ps7_ddrc_0: ps7-ddrc@f8006000 {
    	compatible = "xlnx,ps7-ddrc-1.00.a", "xlnx,ps7-ddrc";
    	reg = < 0xf8006000 0x1000 >;
    	xlnx,has-ecc = <0x0>;
    } ;
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- data/device-tree_v2_1_0.tcl
+++ data/device-tree_v2_1_0.tcl
@@ -3424,6 +3424,7 @@ proc gen_compatible_property {nodename type hw_ver {other_compatibles {}} } {
 		{axi_sysace} {xps_sysace_1.00.a} \
 		{xps_usb_host} {xps_usb_host_1.00.a} \
 		{axi_pcie} {axi_pcie_1.05.a} \
+		{ps7_ddrc} {ps7-ddrc} \
 	]
 
 	if {$hw_ver != ""} {
